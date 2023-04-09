50e0ade020c3bc5abe51faac6d0ccde3a059f762
Derek Hendrickx <derek@hendrickx.me>
2023-04-09 16:23:47 -0400

    Refactor

--- Source/BuildingBlocks/BuildingBlocksGameMode.cpp
+++ Source/BuildingBlocks/BuildingBlocksGameMode.cpp
@@ -7,7 +7,7 @@
 ABuildingBlocksGameMode::ABuildingBlocksGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BuildingBlocks/Blueprints/BP_BuildingBlocksCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
